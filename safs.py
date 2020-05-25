a593e245f70e469f6945bc569f7780bf60c079ed
skyACE <54927177+skyACE@users.noreply.github.com>
2020-05-25 13:27:01 +0800

    Update safs.py

--- safs.py
+++ safs.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 # 右键-run 运行下列代码
-from tool.init_project import *
+#from tool.init_project import *
 
 
 
 # -*- coding: utf-8 -*-
-# 请在terminal窗口输入下方命令：
+# 安装依赖包  请在terminal窗口输入下方命令：
 # pip3 install gy-ui-tools
 
