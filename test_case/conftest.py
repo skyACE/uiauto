0e42c4e0daecdbcbae141853d975d61c2356f336
skyACE <54927177+skyACE@users.noreply.github.com>
2020-09-10 10:16:20 +0800

    判断环境

--- test_case/conftest.py
+++ test_case/conftest.py
@@ -1,12 +1,19 @@
-# -*- coding:utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import pytest
 from tools.ui.base_ui import BaseUI
+import  platform
 
 @pytest.fixture(scope='session')
 def driver():
     base = BaseUI()
-    base.start_browser("chrome")
+    if platform.system() == "Windows":
+        base.start_browser('chrome')
+        print('Window环境运行chrome浏览器驱动')
+    else:       # 无头浏览器 在Linux下
+        base.start_browser('chrome_headless')
+        print('linux环境运行chrome_headless浏览器驱动')
 
     yield base
     base.driver.quit()
